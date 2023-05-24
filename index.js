3a6f3e6d460d1b20774260266edc33866c4d9969
unknown <sureshkrish2104@gmail.com>
2023-05-24 11:10:24 +0530

    changes in msg

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post("/mail", async (req, res) => {
   try {
     const { to, cc, data } = req.body;
     const subject = "Reg: Patient Appointment Details";
-    const text = `Hi Dr.${data.doctorName},\n\n Hope you are doing good.\n\n You have an appointment scheduled with the patient on ${data.appointmentDate}. Please find the details of the patient below:\n\nPatient Name : ${data.name}\nAge : ${data.age}\nGender : ${data.gender}\nComplaints : ${data.complaint}\n\nThank you.\n\nNote: This is an auto generated mail. Please do not reply to this.`;
+    const text = `Hi ${data.doctorName},\n\n Hope you are doing good.\n\n You have an appointment scheduled with the patient on ${data.appointmentDate}. Please find the details of the patient below:\n\nPatient Name : ${data.name}\nAge : ${data.age}\nGender : ${data.gender}\nComplaints : ${data.complaint}\n\nThank you.\n\nNote: This is an auto generated mail. Please do not reply to this.`;
     const mail = await sendMail(to, cc, subject, text);
     if (mail) {
       res.send("Message sent");
